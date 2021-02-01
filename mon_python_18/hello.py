1c4956980ce3e2965337e5f976cff46be370bcd8
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-01 20:04:27 +0200

    Add files via upload

---
+++ mon_python_18/hello.py
@@ -0,0 +1,7 @@
+print('го в маин!')
+
+print('...')
+while True:
+	print('PENTAGON WSLOMAN')
+	print('...')
+
---
+++ mon_python_18/hello2.py
@@ -0,0 +1,21 @@
+#name=input('Введите имя:')
+#print('Привет, '+name+'.')
+
+print(10)
+print(13+20)
+print(13-14)
+print(3*4)
+print(25/5)
+print(20/2+1-5*4)
+print(2**3)
+
+print((2+2)*2)
+
+
+x = 125
+y = 30
+print('125 + 30=', x+y)
+
+number_1=input('Введите число 1 ')
+number_2=input('Введите число 2 ')
+print(int(number_1)+int(number_2))
