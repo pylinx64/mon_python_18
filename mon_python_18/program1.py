8146744afa91b2aaf40a049f2c7fb38cdde33543
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-08 20:01:17 +0200

    Add files via upload

---
+++ mon_python_18/program1.py
@@ -0,0 +1,31 @@
+x = 10
+print(x)
+x = 15
+print(x)
+x = 'молоко'
+print(x)
+
+print(10+10)
+print('10'+'10')
+#print('ачпан ' * 50)
+
+name = 'Alex'
+name_2 = ' капуста'
+name_3 = ' помижор'
+
+print('Alex'+' капуста')
+print(name + name_2 + name_3 )
+
+money = 100
+print(money - 10)
+
+#import random
+#number = random.randint(1, 100)
+#print('рандомное число: ', number)
+
+box = 1
+
+if box == 1:
+	print('Свет вкл')
+else:
+	print('Свет выкл')
