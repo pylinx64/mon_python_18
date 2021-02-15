b054ebac665330a0a1617238616d982a2c61d3ac
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-15 20:02:31 +0200

    Add files via upload

---
+++ mon_python_18/pic_1.py
@@ -0,0 +1,19 @@
+import turtle
+import time
+turtle.bgcolor('black')
+t=turtle.Pen()
+t.shape('turtle')
+#--------------
+t.pencolor('lime')
+t.forward(100)
+t.left(120)
+
+t.pencolor('#29FFCD')
+t.forward(100)
+t.left(120)
+
+t.pencolor('orange')
+t.forward(100)
+t.left(120)
+#--------------
+time.sleep(100)
