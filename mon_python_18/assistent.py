c391abe12ee33c17e489bd21cf44840cb784c1af
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-03-29 20:05:19 +0300

    Add files via upload

---
+++ mon_python_18/assistent.py
@@ -0,0 +1,22 @@
+import pyttsx3
+
+tts = pyttsx3.init()
+
+def sayBots(text):
+    tts.say(text)
+    print(text)
+    tts.runAndWait()
+
+while True:
+    text = input('Введите текст: ')
+
+    if text.lower() == 'привет':
+        sayBots('привет')
+    
+    elif text.lower() == 'как дела?':
+        sayBots('норм')
+    
+        
+    else:
+        sayBots('Я тебя не понимаю')
+    
