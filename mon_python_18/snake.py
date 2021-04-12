cd6021e838a1c74bcada633fe17271c2f74d3904
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-04-12 20:27:19 +0300

    Add files via upload

--- mon_python_18/snake.py
+++ mon_python_18/snake.py
@@ -17,20 +17,49 @@ screen.bgcolor('#31F3FF')
 #--------------------змейка----------------------
 snake=turtle.Turtle()
 snake.shape('square')
+snake.penup()
+
+# туловище
+snake_body = []
 #--------------------змейка----------------------
+
+#--------------------яблоко----------------------
+food=turtle.Turtle()
+food.shape('turtle')
+food.penup()
+food.goto(random.randint(-300, 300), random.randint(-300, 300))
+#--------------------яблоко----------------------
 
 #--------------------управление------------------
 # угол, клавиша
 screen.onkeypress(lambda: snake.setheading(90), 'Up')
-
+screen.onkeypress(lambda: snake.setheading(270), 'Down')
+screen.onkeypress(lambda: snake.setheading(180), 'Left')
+screen.onkeypress(lambda: snake.setheading(0), 'Right')
 screen.listen()
 #--------------------управление------------------
 
 #--------------------игра------------------------
 while True:
+    
+    # кушает еду (если голова касается змеи)
+    if snake.distance(food) < 20:
+        # перемещаем еду
+        food.goto(random.randint(-300, 300), random.randint(-300, 300))
+        # добавляем туловище
+        snake_segment = turtle.Turtle()
+        snake_segment.shape('square')
+        snake_segment.penup()
+        snake_segment.color('#BBBBBB')
+        # ложим кусочек туловища в коробку snake_body
+        snake_body.append(snake_segment)
+    
+    # туловище едет за головой
+        
     # скорость змейки
-    snake.forward(3)
+    snake.forward(5)
     
     # обновление экрана
     screen.update()
-
+    
+    time.sleep(0.01)
